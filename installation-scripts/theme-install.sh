e2a53bb022d4128acbe695cb20d8d7f3e8ce9c82
AmogOSArcolinux <73366837+sanswasnthere26@users.noreply.github.com>
2022-02-20 19:25:38 +0000

    edit the theme script
    old theme script only installs on the hosts machine, this however installs it to the ISO and not on the host users PC

--- installation-scripts/theme-install.sh
+++ installation-scripts/theme-install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-
-cd ~/.themes
+#replace the brackets with the root of the repo example: /home/user/repos/AmogOS-Arch
+cd (~/amogOS-rch)/archiso/airootfs/usr/share/themes
 git clone https://github.com/hacknorris-aka-penguin/amogos-arch-theme.git
