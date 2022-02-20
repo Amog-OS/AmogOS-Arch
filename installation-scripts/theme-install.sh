8a0d2b7b6bcd367d1b0d1d66fd42baa09eb52ed7
AmogOSArcolinux <73366837+sanswasnthere26@users.noreply.github.com>
2022-02-20 19:26:05 +0000

    Whoops i forgot a character aaa

--- installation-scripts/theme-install.sh
+++ installation-scripts/theme-install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 #replace the brackets with the root of the repo example: /home/user/repos/AmogOS-Arch
-cd (~/amogOS-rch)/archiso/airootfs/usr/share/themes
+cd (~/amogOS-Arch)/archiso/airootfs/usr/share/themes
 git clone https://github.com/hacknorris-aka-penguin/amogos-arch-theme.git
