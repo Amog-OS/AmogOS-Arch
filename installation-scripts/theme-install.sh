b04a8596fac07a610db5be16126937ac76eb3698
hacknorris-aka-penguin <97974194+hacknorris-aka-penguin@users.noreply.github.com>
2022-02-20 20:40:45 +0100

    fix
    please finally read where themes should be installed...

--- installation-scripts/theme-install.sh
+++ installation-scripts/theme-install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 #replace the brackets with the root of the repo example: /home/user/repos/AmogOS-Arch
-cd (~/amogOS-Arch)/archiso/airootfs/usr/share/themes
+cd (~/amogOS-Arch)/archiso/airootfs/home/*/.themes/
 git clone https://github.com/hacknorris-aka-penguin/amogos-arch-theme.git
