f7d4fceb66c234289979775cac1d655ac4af556c
Kuna <65683493+kunamech@users.noreply.github.com>
2022-04-08 20:10:35 +0000

    remove redundant `mkdir`

--- installation-scripts/theme-install.sh
+++ installation-scripts/theme-install.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 read -e -p "Please Input the location of the AmogOS-Arch repo." FILEPATH
 echo $FILEPATH
-mkdir $FILEPATH/archiso/airootfs/ect/skel/.local/
-mkdir $FILEPATH/archiso/airootfs/ect/skel/.local/share/
-mkdir $FILEPATH/archiso/airootfs/ect/skel/.local/share/themes/
+mkdir $FILEPATH/archiso/airootfs/ect/skel/.local/ $FILEPATH/archiso/airootfs/ect/skel/.local/share/ $FILEPATH/archiso/airootfs/ect/skel/.local/share/themes/
 cd $FILEPATH/archiso/airootfs/ect/skel/.local/share/themes/
 git clone https://github.com/hacknorris-aka-penguin/amogos-arch-theme.git
